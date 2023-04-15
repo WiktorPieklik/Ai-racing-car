f34067665d531290f581308dd95852a13a729ce8
WiktorPieklik <wiktor.pieklik@icloud.com>
2023-04-15 20:57:41 +0200

    chore: installing script

--- install.py
+++ install.py
@@ -7,5 +7,6 @@ dependencies = [
 ]
 
 if __name__ == "__main__":
+    check_call([executable, '-m', 'pip', 'install', '--upgrade', 'pip', 'setuptools', 'wheel'])
     for dependency in dependencies:
         check_call([executable, '-m', 'pip', 'install', dependency])
