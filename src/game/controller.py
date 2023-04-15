cd61cfe559305acbf8e646d1e24e466ee3d2a4e3
WiktorPieklik <wiktor.pieklik@icloud.com>
2023-04-15 19:55:34 +0200

    feat(controller)!: drawing car radars & hardcore mode

--- src/game/controller.py
+++ src/game/controller.py
@@ -16,9 +16,13 @@ class GameController:
             max_velocity: float = 10.,
             max_angular_velocity: float = 4.,
             max_acceleration: float = .15,
-            max_levels: int = 5
+            max_levels: int = 5,
+            draw_radars: bool = False,
+            hardcore: bool = False
     ):
         self._map_meta = MapMeta(map_type)
+        self._draw_radars = draw_radars
+        self._hardcore = hardcore
         self._state = GameState(max_levels=max_levels)
         self._player_car = PlayerCar(
             max_velocity=max_velocity,
@@ -36,14 +40,21 @@ class GameController:
         pygame.init()
         width = config('WIDTH', cast=int)
         height = config('HEIGHT', cast=int)
+        pygame.display.set_caption("AI racing car")
 
         return pygame.display.set_mode((width, height)), pygame.time.Clock()
 
+    def _reset_player_car(self) -> None:
+        self._player_car.reset(*self._map_meta.car_initial_pos, self._map_meta.car_initial_angle)
+
     def _draw(self) -> None:
         self._window.fill((0, 0, 0))
-        self._window.blit(self._map_meta.track, (0, 0))
-        self._window.blit(self._map_meta.finish_line, (0, 0))
+        if not self._hardcore:
+            self._window.blit(self._map_meta.track, (0, 0))
+            self._window.blit(self._map_meta.finish_line, (0, 0))
         self._player_car.draw(self._window)
+        if self._draw_radars:
+            self._draw_player_car_radars()
 
         lvl_text = MAIN_FONT.render(f"Level {self._state.level}", True, (255, 255, 255))
         time_text = MAIN_FONT.render(f"Time {self._state.level_time():.3f}s", True, (255, 255, 255))
@@ -51,6 +62,13 @@ class GameController:
         self._window.blit(time_text, (10, self._window.get_height() - time_text.get_height() - 20))
 
         pygame.display.update()
+
+    def _draw_player_car_radars(self) -> None:
+        radars_angle = (-60, -30, 0, 30, 60)
+        for angle in radars_angle:
+            line, circle = self._player_car.radar(self._map_meta.track, angle)
+            pygame.draw.line(self._window, *line)
+            pygame.draw.circle(self._window, *circle)
 
     def _player_controls(self) -> None:
         keys = pygame.key.get_pressed()
@@ -100,8 +118,10 @@ class GameController:
             crossed_finish_line_poi = self._player_car.is_colliding(self._map_meta.finish_line_mask)
             if crossed_finish_line_poi:
                 if crossed_finish_line_poi[1] > self._map_meta.finish_line_crossing_point:
+                    print(crossed_finish_line_poi[1])
                     self._player_car.bounce()
                 else:
-                    print("Finishing!")
+                    self._reset_player_car()
+                    self._state.next_level()
 
         pygame.quit()
