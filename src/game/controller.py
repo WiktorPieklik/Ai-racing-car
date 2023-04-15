98796b3558583ff46e69611d4ef43d9b7dc49d59
WiktorPieklik <wiktor.pieklik@icloud.com>
2023-04-15 18:22:56 +0200

    feat(game)!: simple functionality

--- src/game/controller.py
+++ src/game/controller.py
@@ -4,7 +4,7 @@ import pygame
 from decouple import config
 
 from .meta import GameState, MapMeta, MapType
-from .utils import Window, display_text_center
+from .utils import Window, display_text_center, Point
 from .assets import MAIN_FONT
 from .cars import PlayerCar
 
@@ -52,27 +52,56 @@ class GameController:
 
         pygame.display.update()
 
+    def _player_controls(self) -> None:
+        keys = pygame.key.get_pressed()
+        changing_velocity = False
+
+        if keys[pygame.K_LEFT]:
+            self._player_car.rotate(left=True)
+        if keys[pygame.K_RIGHT]:
+            self._player_car.rotate(left=False)
+        if keys[pygame.K_UP]:
+            changing_velocity = True
+            self._player_car.accelerate()
+        if keys[pygame.K_DOWN]:
+            changing_velocity = True
+            self._player_car.decelerate()
+
+        if not changing_velocity:
+            self._player_car.inertia()
+
     def run(self) -> None:
         self._clock.tick(self._fps)
 
         while self._run:
             self._draw()
+            #region game idle & stop
             if not self._state.level_started:
                 display_text_center(self._window, f"Press any key to start {self._state.level} level!", MAIN_FONT)
                 pygame.display.update()
                 while True:
                     keydown = pygame.event.get(pygame.KEYDOWN)
-                    quit = pygame.event.get(pygame.QUIT)
-                    if quit:
+                    should_quit = pygame.event.get(pygame.QUIT)
+                    if should_quit:
                         self._run = False
                         break
                     if keydown:
                         self._state.start_level()
                         break
 
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self._run = False
-                    break
+            if pygame.event.get(pygame.QUIT):
+                self._run = False
+                break
+            #endregion
+            self._player_controls()
+            if self._player_car.is_colliding(self._map_meta.borders_mask):
+                print("Out of the track")
+
+            crossed_finish_line_poi = self._player_car.is_colliding(self._map_meta.finish_line_mask)
+            if crossed_finish_line_poi:
+                if crossed_finish_line_poi[1] > self._map_meta.finish_line_crossing_point:
+                    self._player_car.bounce()
+                else:
+                    print("Finishing!")
 
         pygame.quit()
